5abaaccec4ebb8c4ca56d641f4785f3851bc60c5
Marcel <marcelrg.business@gmail.com>
2025-11-24 01:37:26 +0700

    Modularize script

--- src/scripts/main-new.js
+++ src/scripts/main-new.js
@@ -1,33 +1,22 @@
-const homePage = document.getElementById("homePage");
-const transactionPage = document.getElementById("transactionPage");
-const historyPage = document.getElementById("historyPage");
-const transactionBtn = document.getElementById("transactionBtn");
-const historyBtn = document.getElementById("historyBtn");
-const closeTransactionBtn = document.getElementById("closeTransactionBtn");
-const closeHistoryBtn = document.getElementById("closeHistoryBtn");
-const confirmBtn = document.getElementById("confirmBtn");
-const deleteBtn = document.getElementById("deleteBtn");
-const globalBalanceAmount = document.getElementById("globalBalanceAmount");
-const transactionTypeField = document.getElementById("transactionTypeField");
-const transactionAmountField = document.getElementById("transactionAmountField");
-const transactionHistory = document.getElementById("transactionHistory");
-const errorMessageContainer = document.getElementById("errorMessageContainer");
-const pages = [homePage, transactionPage, historyPage];
-const buttons = [closeHistoryBtn, transactionBtn, historyBtn];
-const transactions = [];
-let globalBalance = 0;
-let isCurrentlyEditing = false;
-let editedTransaction = null;
+class DOMReferences {
+    static homePage = document.getElementById("homePage");
+    static transactionPage = document.getElementById("transactionPage");
+    static historyPage = document.getElementById("historyPage");
+    static transactionBtn = document.getElementById("transactionBtn");
+    static historyBtn = document.getElementById("historyBtn");
+    static closeTransactionBtn = document.getElementById("closeTransactionBtn");
+    static closeHistoryBtn = document.getElementById("closeHistoryBtn");
+    static confirmBtn = document.getElementById("confirmBtn");
+    static deleteBtn = document.getElementById("deleteBtn");
+    static globalBalanceAmount = document.getElementById("globalBalanceAmount");
+    static transactionTypeField = document.getElementById("transactionTypeField");
+    static transactionAmountField = document.getElementById("transactionAmountField");
+    static transactionHistory = document.getElementById("transactionHistory");
+    static errorMessageContainer = document.getElementById("errorMessageContainer");
 
-buttons.forEach((button, index) => {
-    button.onclick = () => {
-        moveToPage(pages[index]);
-    }
-});
-confirmBtn.addEventListener("click", handleConfirm);
-deleteBtn.addEventListener("click", handleDelete);
-closeTransactionBtn.addEventListener("click", handleClose);
-transactionHistory.addEventListener("click", handleHistoryEdit);
+    static pages = [homePage, transactionPage, historyPage];
+    static buttons = [closeHistoryBtn, transactionBtn, historyBtn];
+}
 
 class Transaction {
     static id = 0;
@@ -58,204 +47,268 @@ class Transaction {
     }
 }
 
-function moveToPage(page) {
-    removeAll("active-page");
-    page.classList.add("active-page");
-    page.inert = false;
-}
+class Controller {
+    static isCurrentlyEditing = false;
+    static editedTransaction = null;
+    static createMsg = "Add a new transaction";
+    static editMsg = "Edit a transaction";
+    static incorrectDeletionErrorMsg = "Cannot delete outside transaction editor:Press the close button to close this page";
+    static overSpendingErrorMsg = "Cannot spend more than current balance";
+    static overSpendingDeletionErrorMsg = "Cannot delete:Balance cannot be less than 0";
+    static incorrectSyntaxErrorMsg = "Amount must be a number:Amount must be at least 1";
 
-function removeAll(className) {
-    pages.forEach(page => {
-        page.classList.remove(className);
-        page.inert = true;
-    });
-}
+    static initializeEventListeners() {
+        DOMReferences.buttons.forEach((button, index) => {
+            button.onclick = () => {
+                View.moveToPage(DOMReferences.pages[index]);
+            }
+        });
 
-function resetTransactionPage() {
-    transactionAmountField.value = null;
-    transactionTypeField.value = "income";
-    transactionPage.querySelector("h2").textContent = "Add a new transaction";
-}
-
-function validateTransaction(type, amount) {
-    const errorObject = {
-        isValid: true,
-        message: "",
+        DOMReferences.confirmBtn.addEventListener("click", Controller.handleConfirm);
+        DOMReferences.deleteBtn.addEventListener("click", Controller.handleDelete);
+        DOMReferences.closeTransactionBtn.addEventListener("click", Controller.handleClose);
+        DOMReferences.transactionHistory.addEventListener("click", Controller.handleHistoryEdit);
     }
 
-    if(amount <= 0 || isNaN(amount)) {
-        errorObject.message = "Amount must be a number:Amount must be at least 1";
-        errorObject.isValid = false;
-    }
-    if(type === "expense" && amount > globalBalance) {
-        errorObject.message = "Cannot spend more than current balance";
-        errorObject.isValid = false;
-    }
+    static validateTransaction(type, amount) {
+        const errorObject = {
+            isValid: true,
+            message: "",
+        }
 
-    return errorObject;
-}
+        if(amount <= 0 || isNaN(amount)) {
+            errorObject.message = Controller.incorrectSyntaxErrorMsg;
+            errorObject.isValid = false;
+        }
+        if(type === "expense" && amount > Model.globalBalance) {
+            errorObject.message = Controller.overSpendingErrorMsg;
+            errorObject.isValid = false;
+        }
 
-function updateBalance(transaction) {
-    if(transaction.type === "income") {
-        globalBalance += transaction.amount;
-    } else {
-        globalBalance -= transaction.amount;
-    }
-
-    updateGlobalBalanceView();
-}
-
-function revertBalance(transaction) {
-    if(transaction.type === "income") {
-        globalBalance -= transaction.amount;
-    } else {
-        globalBalance += transaction.amount;
+        return errorObject;
     }
 
-    updateGlobalBalanceView();
-}
-
-function updateGlobalBalanceView() {
-    globalBalanceAmount.textContent = `Balance: $${globalBalance}`;
-}
-
-function displayError(messages) {
-    errorMessageContainer.textContent = null;
-    errorMessageContainer.style.display = "block";
-    errorMessageContainer.hidden = false;
-    const messagesArr = messages.split(":");
-    messagesArr.forEach(message => {
-        const errorMessage = document.createElement("p");
-        errorMessage.classList.add("errorMessage");
-        errorMessage.textContent = message;
-        errorMessageContainer.appendChild(errorMessage);
-    })
-}
-
-function removeErrors() {
-    errorMessageContainer.style.display = "none";
-    errorMessageContainer.hidden = true;
-    errorMessageContainer.textContent = null;
-}
-
-function createTransactionHistory(transaction) {
-    const transactionElement = document.createElement("button");
-    const amountElement = document.createElement("div");
-    const dateElement = document.createElement("div");
-    const timeElement = document.createElement("div");
-    transactionElement.classList.add("previousTransaction");
-    amountElement.id = transaction.id;
-    amountElement.classList.add("previousTransactionAmount");
-    dateElement.classList.add("previousTransactionDate");
-    timeElement.classList.add("previousTransactionTime");
-    if(transaction.type === "income") {
-        amountElement.textContent = `+$${transaction.amount}`; 
-    } else {
-        amountElement.textContent = `-$${transaction.amount}`;
+    static handleConfirm() {
+        View.removeErrors();
+        if(Controller.isCurrentlyEditing) {
+            const oldAmount = Controller.editedTransaction.amount;
+            const oldType = Controller.editedTransaction.type;
+            const newAmount = Number(DOMReferences.transactionAmountField.value);
+            const newType = DOMReferences.transactionTypeField.value;
+            const errStatus = Model.editBalance(oldType, newType, oldAmount, newAmount);
+            if(!errStatus) {
+                View.displayError(Controller.overSpendingErrorMsg);
+            } else {
+                View.editTransactionMessage(Controller.editedTransaction.id, newType, newAmount);
+                View.updateBalance(Model.globalBalance);
+                View.resetTransactionPage();
+                View.moveToPage(DOMReferences.homePage);
+                Controller.editedTransaction.type = newType;
+                Controller.editedTransaction.amount = newAmount;
+                Controller.isCurrentlyEditing = false;
+                Controller.editedTransaction = null;
+            }
+        } else {
+            const type = DOMReferences.transactionTypeField.value;
+            const amount = Number(DOMReferences.transactionAmountField.value);
+            const errorObject = Controller.validateTransaction(type, amount);
+            if(!errorObject.isValid) {
+                View.displayError(errorObject.message);
+                return false;
+            }
+            const transaction = new Transaction(type, amount);
+            Controller.createTransactionHistory(transaction);
+            Model.transactions.push(transaction);
+            Model.updateBalance(transaction);
+            View.updateBalance(Model.globalBalance);
+            View.resetTransactionPage();
+            View.moveToPage(DOMReferences.homePage);
+        }
     }
-    dateElement.textContent = transaction.date;
-    timeElement.textContent = transaction.time;
-    transactionElement.appendChild(amountElement);
-    transactionElement.appendChild(dateElement);
-    transactionElement.appendChild(timeElement);
-    transactionHistory.appendChild(transactionElement);
+
+    static handleDelete() {
+        View.removeErrors();
+        if(Controller.isCurrentlyEditing) {
+            const temp = Model.globalBalance;
+            Model.revertBalance(Controller.editedTransaction);
+            if(Model.globalBalance < 0) {
+                Model.globalBalance = temp;
+                View.displayError(Controller.overSpendingDeletionErrorMsg);
+                View.updateBalance(Model.globalBalance);
+                return false;
+            }
+            View.updateBalance(Model.globalBalance);
+            View.deleteTransaction(Controller.editedTransaction.id);
+            View.resetTransactionPage();
+            View.moveToPage(DOMReferences.homePage);
+            Controller.isCurrentlyEditing = false;
+            Controller.editedTransaction = null;
+        } else {
+            View.displayError(Controller.incorrectDeletionErrorMsg);
+        }
+    }
+
+    static handleClose() {
+        if(Controller.isCurrentlyEditing) {
+            Controller.isCurrentlyEditing = false;
+            Controller.editedTransaction = null;
+        }
+        View.removeErrors();
+        View.resetTransactionPage();
+        View.moveToPage(DOMReferences.homePage);
+    }
+
+    static handleHistoryEdit(event) {
+        if(event.target.matches(".previousTransactionAmount")) {
+            const transaction = Model.findTransaction(Number(event.target.id));
+            View.moveToPage(DOMReferences.transactionPage);
+            View.changeTransactionPageHeading(Controller.editMsg);
+            DOMReferences.transactionTypeField.value = transaction.type;
+            DOMReferences.transactionAmountField.value = transaction.amount;
+
+            Controller.isCurrentlyEditing = true;
+            Controller.editedTransaction = transaction;
+        } else {
+            return false;
+        }
+    }
+
+    static createTransactionHistory(transaction) {
+        const transactionElement = document.createElement("button");
+        const amountElement = document.createElement("div");
+        const dateElement = document.createElement("div");
+        const timeElement = document.createElement("div");
+        transactionElement.classList.add("previousTransaction");
+        amountElement.id = transaction.id;
+        amountElement.classList.add("previousTransactionAmount");
+        dateElement.classList.add("previousTransactionDate");
+        timeElement.classList.add("previousTransactionTime");
+        if(transaction.type === "income") {
+            amountElement.textContent = `+$${transaction.amount}`; 
+        } else {
+            amountElement.textContent = `-$${transaction.amount}`;
+        }
+        dateElement.textContent = transaction.date;
+        timeElement.textContent = transaction.time;
+        transactionElement.appendChild(amountElement);
+        transactionElement.appendChild(dateElement);
+        transactionElement.appendChild(timeElement);
+        View.renderElement(DOMReferences.transactionHistory, transactionElement);
+    }
 }
 
-function handleConfirm() {
-    removeErrors();
-    if(isCurrentlyEditing) {
-        const oldAmount = editedTransaction.amount;
-        const oldType = editedTransaction.type;
-        const newAmount = Number(transactionAmountField.value);
-        const newType = transactionTypeField.value;
+class View {
+    static moveToPage(page) {
+        View.removeAll("active-page");
+        page.classList.add("active-page");
+        page.inert = false;
+    }
+
+    static removeAll(className) {
+        DOMReferences.pages.forEach(page => {
+            page.classList.remove(className);
+            page.inert = true;
+        });
+    }
+
+    static resetTransactionPage() {
+        DOMReferences.transactionAmountField.value = null;
+        DOMReferences.transactionTypeField.value = "income";
+        DOMReferences.transactionPage.querySelector("h2").textContent = "Add a new transaction";
+    }
+
+    static changeTransactionPageHeading(message) {
+        DOMReferences.transactionPage.querySelector("h2").textContent = message;
+    }
+    
+    static updateBalance(balance) {
+        DOMReferences.globalBalanceAmount.textContent = `Balance: $${balance}`;
+    }
+
+    static displayError(messages) {
+        DOMReferences.errorMessageContainer.textContent = null;
+        DOMReferences.errorMessageContainer.style.display = "block";
+        DOMReferences.errorMessageContainer.hidden = false;
+        const messagesArr = messages.split(":");
+        messagesArr.forEach(message => {
+            const errorMessage = document.createElement("p");
+            errorMessage.classList.add("errorMessage");
+            errorMessage.textContent = message;
+            this.renderElement(DOMReferences.errorMessageContainer, errorMessage);
+        })
+    }
+
+    static removeErrors() {
+        DOMReferences.errorMessageContainer.style.display = "none";
+        DOMReferences.errorMessageContainer.hidden = true;
+        DOMReferences.errorMessageContainer.textContent = null;
+    }
+
+    static renderElement(parent, child) {
+        parent.appendChild(child);
+    }
+
+    static deleteTransaction(id) {
+        document.getElementById(id).closest(".previousTransaction").style.display = "none";
+        document.getElementById(id).closest(".previousTransaction").hidden = true;
+    }
+
+    static editTransactionMessage(id, type, amount) {
+        if(type === "income") {
+            document.getElementById(id).textContent = `+$${amount}`;
+        } else {
+            document.getElementById(id).textContent = `-$${amount}`;
+        }
+    }
+}
+
+class Model {
+    static transactions = [];
+    static globalBalance = 0;
+
+    static updateBalance(transaction) {
+        if(transaction.type === "income") {
+            Model.globalBalance += transaction.amount;
+        } else {
+            Model.globalBalance -= transaction.amount;
+        }
+    }
+
+    static revertBalance(transaction) {
+        if(transaction.type === "income") {
+            Model.globalBalance -= transaction.amount;
+        } else {
+            Model.globalBalance += transaction.amount;
+        }
+    }
+
+    static findTransaction(id) {
+        return Model.transactions.find(object => object.id === id);
+    }
+
+    static editBalance(oldType, newType, oldAmount, newAmount) {
         const amountDifference = oldAmount - newAmount;
         if(newType === "income") {
             if(newType !== oldType) {
-                globalBalance += (newAmount + oldAmount);
+                Model.globalBalance += (newAmount + oldAmount);
             } else {
-                globalBalance -= amountDifference;
+                Model.globalBalance -= amountDifference;
             }
-            document.getElementById(editedTransaction.id).textContent = `+$${newAmount}`;
         } else {
-            const temp = globalBalance;
+            const temp = Model.globalBalance;
             if(newType !== oldType) {
-                globalBalance -= (newAmount + oldAmount);
+                Model.globalBalance -= (newAmount + oldAmount);
             } else {
-                globalBalance += amountDifference;
+                Model.globalBalance += amountDifference;
             }
-            if(globalBalance < 0) {
-                displayError("Unable to spend more than current balance");
-                globalBalance = temp;
+            if(Model.globalBalance < 0) {
+                Model.globalBalance = temp;
                 return false;
             }
-            document.getElementById(editedTransaction.id).textContent = `-$${newAmount}`;
         }
-        editedTransaction.type = newType;
-        editedTransaction.amount = newAmount;
-        updateGlobalBalanceView();
-        resetTransactionPage();
-        moveToPage(homePage);
-        isCurrentlyEditing = false;
-        editedTransaction = null;
-    } else {
-        const type = transactionTypeField.value;
-        const amount = Number(transactionAmountField.value);
-        const errorObject = validateTransaction(type, amount);
-        if(!errorObject.isValid) {
-            displayError(errorObject.message);
-            return false;
-        }
-        const transaction = new Transaction(type, amount);
-        transactions.push(transaction);
-        updateBalance(transaction);
-        createTransactionHistory(transaction);
-        resetTransactionPage();
-        moveToPage(homePage);
+
+        return true;
     }
 }
 
-function handleDelete() {
-    removeErrors();
-    if(isCurrentlyEditing) {
-        const temp = globalBalance;
-        revertBalance(editedTransaction);
-        if(globalBalance < 0) {
-            displayError("Cannot delete:Balance cannot be less than 0");
-            globalBalance = temp;
-            updateGlobalBalanceView();
-            return false;
-        }
-        document.getElementById(editedTransaction.id).closest(".previousTransaction").style.display = "none";
-        document.getElementById(editedTransaction.id).closest(".previousTransaction").hidden = true;
-        resetTransactionPage();
-        moveToPage(homePage);
-        isCurrentlyEditing = false;
-        editedTransaction = null;
-    } else {
-        displayError("Cannot delete outside transaction editor:Press the close button to close this page");
-    }
-}
-
-function handleClose() {
-    removeErrors();
-    if(isCurrentlyEditing) {
-        isCurrentlyEditing = false;
-        editedTransaction = null;
-    }
-    resetTransactionPage();
-    moveToPage(homePage);   
-}
-
-function handleHistoryEdit(event) {
-    if(event.target.matches(".previousTransactionAmount")) {
-        moveToPage(transactionPage);
-        transactionPage.querySelector("h2").textContent = "Edit a transaction";
-        const transaction = transactions.find(object => object.id === Number(event.target.id));
-        transactionTypeField.value = transaction.type;
-        transactionAmountField.value = transaction.amount;
-        isCurrentlyEditing = true;
-        editedTransaction = transaction;   
-    } else {
-        return false;
-    }
-}
+Controller.initializeEventListeners();
